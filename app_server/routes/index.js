6532bf7f6249b86529bf8020373cc3bf4646b7b2
21eg106a06 <21eg106a06@anurag.edu.in>
2023-08-10 09:46:01 +0530

    First commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controller/locations');
+var ctrlOthers = require('../controller/others');
+
+
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location2',ctrlLocations.locationInfo2);
+router.get('/location3',ctrlLocations.locationInfo3);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+
+router.get('/about',ctrlOthers.about);
+
+module.exports = router;
